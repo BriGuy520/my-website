09fb593620d7ffff48c673e8158e936b506d4ed2
Brian Falasz <brianfalasz@gmail.com>
2018-11-12 17:54:08 -0600

    added a little css color to the Goals component

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import '../styles/Goals.css';
 
 
-const Goals = props => {
+const Goals = (props) => {
 
   return (
-    <h1>My Goals</h1>
+    <div className="goals">
+      <h1>My Goals</h1>
+      <p>Below is a list of goals I would like to accomplish with not just this website, but with my
+      web development career in general:
+      </p>
+    </div>
   );
 }
 
