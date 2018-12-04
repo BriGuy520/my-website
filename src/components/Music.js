92da5ad538b850110387e8c6e9ccea1dd86e99c3
Brian Falasz <brianfalasz@gmail.com>
2018-12-04 14:26:19 -0600

    begrudgingly added a music player to Music component. Wanted to use the soundcloud API. But, that is not available to use at the moment.

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../styles/Music.css';
+
+
 
 const Music = () => {
   return (
-    <div>Music</div>
+    <div className='playlist'>
+      <iframe width="100%" height="100%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3468921&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" title="myMusic"></iframe>
+    </div>
   )
 }
 
