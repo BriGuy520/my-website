bccae350d8d2e5c34918cb0871dd8f2a66eb6392
Brian Falasz <brianfalasz@gmail.com>
2018-12-05 13:22:13 -0600

    added some state functionality to the goals section of my homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/Home.css';
 
 
-const Home = () => {
-  return (
-    <div>
-      <div className="cover">
-        <h1>Hi Everybody!</h1>
+class Home extends Component {
+
+  state = { goals: ['Get a development gig'] };
+
+
+  handleNewGoal = (event) => {
+    const { goals } = this.state;
+    this.setState({ goals: [...goals, '']});
+  }
+
+  handleGoalChange = (event) => {
+    const index = Number(event.target.name.split('-')[1]);
+    const goals = this.state.goals.map((goal, idx) => {
+      return idx === index ? event.target.value : goal;
+    });
+
+    this.setState({goals});
+
+  } 
+
+  render(){
+
+    const { goals } = this.state;
+
+    const goalInput = goals.map((goal, idx) => {
+      return (
+        <div className="goal-input" key={idx}>
+          <label>{idx +  1}</label>
+          <input type="text" name={`goal-${idx}`} value={goal} autoComplete="off" onChange={this.handleGoalChange.bind(this)} />
+        </div>
+      );
+    });
+  
+    return (
+      <div>
+        <div className="cover">
+          <h1>Hi Everybody!</h1>
+        </div>
+        <div className='goals'>
+          <h1>My Goals</h1>
+          <p>Below is a list of goals I would like to accomplish with not just this website, but with my
+          web development career in general:
+          </p>
+          <div className="goal-input">
+            <form onSubmit={this.handleSubmit}>
+              <button type='button' onClick={this.handleNewGoal.bind(this)} className="buttons">Add Goal</button>
+            </form>
+          </div>
+          <ol>
+            {goalInput}
+          </ol>
+        </div>
       </div>
-      <div className='goals'>
-        <h1>My Goals</h1>
-        <p>Below is a list of goals I would like to accomplish with not just this website, but with my
-        web development career in general:
-        </p>
-        <ul>
-          <li>Goal 1</li>
-        </ul>
-      </div>
-    </div>
-   
-  );
+     
+    );
+  }
 }
 
 export default Home;
--- src/components/Music.js
+++ src/components/Music.js
@@ -6,7 +6,7 @@ import '../styles/Music.css';
 const Music = () => {
   return (
     <div className='playlist'>
-      <iframe width="100%" height="100%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3468921&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" title="myMusic"></iframe>
+      <iframe width="100%" height="95%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/3468921&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true" title="soundcloudPlayer"></iframe>
     </div>
   )
 }
