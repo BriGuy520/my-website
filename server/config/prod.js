8afe093c09957900860d6f654f819088d4179793
Brian Falasz <brianfalasz@gmail.com>
2019-01-03 19:55:48 -0600

    added production keys for oauth

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,10 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  twitterClientID: process.env.TWITTER_CLIENT_ID,
+  twitterClientSecret: process.env.TWITTER_CLIENT_SECRET,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET
 }
