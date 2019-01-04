c994965cf9cc9042ebf129db65933dd16293b18c
Brian Falasz <brianfalasz@gmail.com>
2019-01-04 13:34:05 -0600

    setup authReducer and fetchUser action creator

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import photosReducer from './photosReducer';
+import authReducer from './authReducer';
 
 
 export default combineReducers({
   photos: photosReducer,
+  auth: authReducer
 });
