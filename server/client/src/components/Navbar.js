44d6a4a2f2510960907806434a88e2a30686f6a2
Brian Falasz <brianfalasz@gmail.com>
2019-02-18 22:20:45 -0600

    made some styling to mobile layout

--- server/client/src/components/Navbar.js
+++ server/client/src/components/Navbar.js
@@ -3,21 +3,25 @@ import { Link } from 'react-router-dom';
 import'../styles/Navbar.css';
 
 
-class Navbar extends Component {
-  render(){
-    return (
-      <header>
+const Navbar = () => {
+
+
+
+  return (
+    <header>
+      <div className="dropdown-menu">
         <h2><Link to="/">Brian Falasz</Link></h2>
-        <nav className="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/projects">Projects</Link></li>
-          <li><Link to="/music">Music</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
-          <li><Link to="/pictures">Pictures</Link></li>
-        </nav>
-      </header>
-    );
-  }
+        <i className="bars icon"></i>
+      </div>
+      <nav className="nav">
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+        <li><Link to="/music">Music</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/pictures">Pictures</Link></li>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
--- server/client/src/components/home/Skills.js
+++ server/client/src/components/home/Skills.js
@@ -3,55 +3,73 @@ import React from 'react';
 const Skills = () => {
   return (
     <div className="skills"> 
-      <div className="skill-section">
-        <div className="section-headers">
-          <i className="desktop icon"></i>
-          <h3>Frontend</h3>
-        </div>
-        <ul>
-          <li>HTML/CSS</li>
-          <li>Javascript</li>
-          <li>React</li>
-          <li>Redux</li>
-        </ul>
+      <div className="top-section">
+        <h1>Skills</h1>
+        <p>Here are a few things I know and wish to know in the near to long term future.</p>
       </div>
-      <div className="skill-section">
-        <div>
+      <div className="">
+        <div className="skill-section">
           <div className="section-headers">
-            <i className="road icon"></i>
-            <h3>Languages/Frameworks I want to Learn in the Near Future:</h3> 
+            <div className="section-icon">
+              <i className="desktop icon"></i>
+            </div>
           </div>
-          <ol>
-            <li>Angular</li>
-            <li>Ember</li>
-            <li>Vue</li>
-            <li>Python</li>
-            <li>PHP</li>
-            <li>Ruby/Rails</li>
-          </ol>
+          <div className="skill-list">
+            <h3>Frontend</h3>
+            <ul>
+              <li>HTML/CSS</li>
+              <li>Javascript</li>
+              <li>React</li>
+              <li>Redux</li>
+            </ul>
+            <div className="section-border"></div>
+          </div>
         </div>
-        <div>
+        <div className="skill-section">
+          <div>
+            <div className="section-headers">
+              <div className="section-icon">
+                <i className="road icon"></i>
+              </div>
+              <h3>Languages/Frameworks I want to Learn in the Near Future:</h3> 
+            </div>
+            <ol>
+              <li>Angular</li>
+              <li>Ember</li>
+              <li>Vue</li>
+              <li>Python</li>
+              <li>PHP</li>
+              <li>Ruby/Rails</li>
+            </ol>
+          </div>
+          <div>
+            <div className="section-headers">
+              <div className="section-icon">
+                <i className="rocket icon"></i>
+              </div>
+              <h3>Languages/Frameworks I want to Learn in the Distant Future:</h3>
+            </div>
+            <ol>
+              <li>C++</li>
+              <li>Go</li>
+            </ol>
+          </div>
+          <div className="section-border"></div>
+        </div>
+        <div className="skill-section">
           <div className="section-headers">
-            <i className="rocket icon"></i>
-            <h3>Languages/Frameworks I want to Learn in the Distant Future:</h3>
+            <div className="section-icon">
+              <i className="sitemap icon"></i>
+            </div>
+            <h3>Backend</h3>
           </div>
-          <ol>
-            <li>C++</li>
-          </ol>
+          <ul>
+            <li>MongoDB</li>
+            <li>NodeJS</li>
+            <li>Express</li>
+            <li>Mongoose</li>
+          </ul>
         </div>
-        
-      </div>
-      <div className="skill-section">
-        <div className="section-headers">
-          <i class="sitemap icon"></i>
-          <h3>Backend</h3>
-        </div>
-        <ul>
-          <li>MongoDB</li>
-          <li>NodeJS</li>
-          <li>Express</li>
-          <li>Mongoose</li>
-        </ul>
       </div>
     </div>
   );
