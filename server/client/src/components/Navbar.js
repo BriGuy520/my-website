c295f27ba93b849601afa953bc0062917d315c69
Brian Falasz <brianfalasz@gmail.com>
2019-02-19 19:51:26 -0600

    fixed navbar so that it adjusts to different screen sizes

--- server/client/src/components/Navbar.js
+++ server/client/src/components/Navbar.js
@@ -7,10 +7,25 @@ class Navbar extends Component {
   constructor(props){
     super(props);
 
-    this.state = { showMenu: false }
+    this.state = { showMenu: false, width: 0, height: 0 };
 
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     this.showMenu = this.showMenu.bind(this);
     this.closeMenu = this.closeMenu.bind(this); 
+  }
+
+  componentDidMount(){
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount(){
+    this.updateWindowDimensions();
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions(){
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
 
   showMenu(event){
@@ -68,9 +83,7 @@ class Navbar extends Component {
               </nav>
 
             ) : (
-
               null
-
             )
           }
         </header>
