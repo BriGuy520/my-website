f334d4c32f425a78cf46b9882810a173ecc873fe
Brian Falasz <brianfalasz@gmail.com>
2019-02-21 22:46:12 -0600

    added more styling to the about page

--- server/client/src/components/about/Favorites.js
+++ server/client/src/components/about/Favorites.js
@@ -59,22 +59,22 @@ const Favorites = () => {
               <h3>Movies</h3>
               <ol>
               <img className="book-img" src={heat} />
-                <li><strong>Heat</strong> Written and Directed by Michael Mann</li>
+                <li><strong>Heat</strong> <br/>Written and Directed by Michael Mann</li>
                 <img className="book-img" src={adaptation} />
-                <li><strong>Adaptation.</strong> Written by Charlie Kaufman Directed by Spike Jonze</li>
+                <li><strong>Adaptation.</strong> <br/>Written by Charlie Kaufman and Directed by Spike Jonze</li>
                 <img className="book-img" src={boogie_nights} />
-                <li><strong>Boogie Nights</strong> Written and Directed by Paul Thomas Anderson</li>
+                <li><strong>Boogie Nights</strong> <br/>Written and Directed by Paul Thomas Anderson</li>
               </ol>
             </div>
             <div className="list-sections">
               <h3>TV Shows</h3>
               <ol>
                 <img className="book-img" src={simpsons} />
-                <li><strong>The Simpsons</strong> Written and Directed by Michael Mann</li>
+                <li><strong>The Simpsons</strong> <br/>Created by Matt Groening, Sam Simon and James L. Brooks</li>
                 <img className="book-img" src={wire} />
-                <li><strong>The Wire</strong> Written by Charlie Kaufman Directed by Spike Jonze</li>
+                <li><strong>The Wire</strong> <br/>Created by David Simon</li>
                 <img className="book-img" src={south_park} />
-                <li><strong>South Park</strong> Written and Directed by Paul Thomas Anderson</li>
+                <li><strong>South Park</strong> <br/>Created by Trey Parker and Matt Stone</li>
               </ol>
             </div>
           </div>
@@ -87,10 +87,10 @@ const Favorites = () => {
         <h2>Music</h2>
         <div className="list-sections">
           <ol>
-            <li><strong>Lateralus</strong> Tool</li>
-            <li><strong>Cross</strong> Justice</li>
-            <li><strong>Mezanine</strong> Massive Attack</li>
-            <li><strong>Black on Both Sides</strong> Mos Def</li>
+            <li><strong>Lateralus</strong> by Tool</li>
+            <li><strong>Cross</strong> by Justice</li>
+            <li><strong>Mezanine</strong> by Massive Attack</li>
+            <li><strong>Black on Both Sides</strong> by Mos Def</li>
           </ol>
         </div>
       </div>
