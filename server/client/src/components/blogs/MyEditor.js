d6e2370e9e9b79fb5dccb8decf31b73fff139eda
Brian Falasz <brianfalasz@gmail.com>
2019-02-25 15:37:34 -0600

    added some customization for my wysisyg toolbar

--- server/client/src/components/blogs/MyEditor.js
+++ server/client/src/components/blogs/MyEditor.js
@@ -3,8 +3,31 @@ import ReactQuill from 'react-quill';
 
 const MyEditor = ({ input }) => {
 
+
+  const modules = {
+    toolbar: [
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      ['bold', 'italic', 'underline','strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image'],
+      [{'color': [] }, { 'background': [] }],
+      [{'font': [] }],
+      [{'align': [] }],
+      ['clean']
+    ],
+  },
+
+  formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ]
+
   return (
     <ReactQuill 
+      modules={modules}
+      formats={formats}
       {...input}
       onChange={(newValue, delta, source) => {
         if(source === 'user'){
