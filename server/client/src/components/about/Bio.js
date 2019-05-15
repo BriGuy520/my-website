59881dcebae2e7614fc986b618b62d27039aaed4
Brian Falasz <bfalasz@yahoo.com>
2019-05-15 15:51:44 -0500

    modified my bio to make it less wordy and more concise

--- server/client/src/components/about/Bio.js
+++ server/client/src/components/about/Bio.js
@@ -3,49 +3,21 @@ import React from 'react';
 const Bio = () => {
   return (
     <div className="bio">
-        <h1>A Lot About Myself...</h1>
-        <p>
-          I was born Chicago, IL and raised in the northern suburb of Libertyville, IL. I went to Libertyville High School and then to the Southern Illinois University-Carbondale. I fully admit, I was not ready for college at the time. Although I graduated with a degree in Business Management, I was still very immature and unsure of what it is that I truly wanted to do by the time I graduated in 2012. For a few years after college, I have worked for a number of great companies such as Whole Foods, Chipotle, Walgreens and STATS LLC. While I learned a great deal and met a ton of amazing people through these jobs, I've always had the desire to do something bigger, something that would allow me to reach my full potential. That's when I started to take an interest in programming.
-        </p>
-        <p>
-          I started with learning the basics at codeacademy.com. I dabbled with html and css and thought that it would be easy as pie to learn how to code. Boy was I wrong. The first language that I attempted was Javascript. Coming into the subject with very little experience, I struggled mightly. Just understanding the basics, like variables and functions, was extremely difficult to grasp. After struggling for a couple of months, I came across this new thing popping up all over the country called Coding Bootcamps.  
-        </p>
-        <p>
-          After looking into it more, I thought that I had found my answer. Go to a bootcamp, pay them tens of thousands of dollars and come out a professional developer in only 3 months. A very alluring prospect for a typical millenial like myself. So, after deciding on a bootcamp in Chicago, I reversed course and decided to learn Ruby. The reason for this was simple, that's what the bootcamp was going to teach me. Being so gung-ho about the prospect of landing a professional gig in 3 months, I spent hours trying to understand the concepts, but was still having a hard time grasping the material at codeacademy. Accompanied by the naive assumption that if I just kept pushing forward I would just get it and that I needed to learn as much as possible, as fast as possible in order to get a job. The problem with that was that the further I went and the more complex the concepts got the more lost I was. 
-        </p>
-        <p>
-          However, that didn't deter me from applying. It probably should have.
-        </p>
-        <p>
-          After only a month of learning Ruby, I put in my application in for admittance to the bootcamp. Part of the application process was having a technical interview. To say I did poorly would be an understatement. I struggled with the most basic things like calling a function and producing a simple counting algorithm. In turn, I was rejected for admittance. 
-        </p>
-        <p>
-          Completely distraught about the rejection, I came to think that I didn't have what it takes to be a professional programmer. I pretty much gave up on the idea and started thinking of other things that I would be interested in. I even went back to school for a little bit with the intention of getting my Masters in Finance. Yeah, no. 
-        </p>
-        <p>
-          After a year of entertaining the idea of doing other practical things with my life like writing screenplays, I was stuck. I thought my life was destined to working jobs that weren't fullfilling and living paycheck to paycheck just to get by. Then one night, working an overnight shift at Walgreens, hating my life, I decided that I needed to grow a pair and take up coding again. 
-        </p>
-        <p>
-          That was a year ago. 
-        </p>
-        <p>
-          Today, after many months of hard work and determination, I honestly believe that I have what it takes to be a professional developer. It has been frustrating at times, there are times when I wanted to give up... Again. However, I am a lot more mature now and have learned to persevere through the struggles and to continue my goal of being a professional web developer. 
-        </p>
-        <p>
-          The reason I wrote such a long-winded, bloviated bio is for a couple of reasons.
-        </p>
-        <p>
-          First, I want to use my story as an example to show that an ordinary person of average ability can achieve great things if you have the sincere drive to become better and work towards your goals. It takes a hell of a lot of sacrifice and a ton of frustration and self doubt. But, if you really want something you have to earn it through hard work. There is no way around it. There are no shortcuts to true success. 
-        </p>
-        <p>
-          Second, that no two paths are the same. Looking back, I am truly grateful for the rejection I recieved from the bootcamp. Bootcamps might be a great resource for some people to learn how to become great programmers, but they're not for everybody. They're very expensive and quite intense, hence the name. From my experience, coding is not something the vast majority of people can just pick up in 3 months. It takes years of continuous education to master. Even after that, you're not ever truly a master. The industry moves so quickly that you have to constantly be updating your skills and learning new technologies. Remember when jQuery was the hottest framework?
-        </p>
-        <p>
-          I hope that this will be read by somone who is in the shoes I was in a couple of years ago. Even if your goals are completely seperate than mine, just please, never give up and never lose hope. The life you want is within your reach. It just depends on how badly you want it.
-        </p>
-        <p> 
-          This has been my story.
-        </p>
+        <h1>A Little About Myself...</h1>
+          <p>
+            Hey, 
+          </p>
+          <p>
+            My name is Brian and I'm a web developer based out of Austin, TX. I'm entirely self taught and have been doing web development for a over a year now. 
+            My main focus has been on Front End Development with React and Redux being my go to framework. However, I've dabbled with other popular frameworks such
+            as Angular and Vue.js. 
+          </p>
+          <p>
+            I also do some backend and database work with Node.js being my go to backend langauge and MongoDB as go to database solution. 
+          </p>
+          <p>
+            I've also worked with other langauges such as PHP and relational databases such as mySQL.
+          </p>
         <div className="section"></div>
         <br />
       </div>
