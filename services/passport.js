fb4ed0567432b80ba0d676db7951aeaeae5b3617
Brian Falasz <bfalasz@yahoo.com>
2019-06-05 10:30:37 -0500

    deploying verision 1.0.0 of my site

--- services/passport.js
+++ services/passport.js
@@ -3,9 +3,9 @@ const FacebookStrategy = require('passport-facebook');
 const TwitterStrategy = require('passport-twitter');
 const GoogleStrategy = require('passport-google-oauth20');
 const GitHubStrategy = require('passport-github');
-// const LocalStrategy = require('passport-local');
-// const JwtStrategy = require('passport-jwt').Strategy;
-// const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const User = mongoose.model('User');
@@ -21,41 +21,41 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-// const localLogin = new LocalStrategy(function(username, password, done){
-//   User.findOne({ local: { username: username } },  function(err, user){
-//     console.log(user);
+const localLogin = new LocalStrategy(function(username, password, done){
+  User.findOne({ local: { username: username } },  function(err, user){
+    console.log(user);
     
-//     if(err) { return done(err) }
+    if(err) { return done(err) }
   
-//     user.comparePassword(password, (err, isMatch) => {
-//       if(err) { return done(err); }
-//       if(!isMatch){ return done(null, false); }
+    user.comparePassword(password, (err, isMatch) => {
+      if(err) { return done(err); }
+      if(!isMatch){ return done(null, false); }
 
-//       return done(null, user);
-//     }); 
-//   });
-// });
+      return done(null, user);
+    }); 
+  });
+});
 
-// const jwtOptions = { 
-//   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-//   secretOrKey: keys.jwtSecret
-// }
+const jwtOptions = { 
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secretOrKey: keys.jwtSecret
+}
 
-// const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
-//   User.findById(payload.sub, function(err, user){
-//     if(err){ return done(err, false); }
+  User.findById(payload.sub, function(err, user){
+    if(err){ return done(err, false); }
 
-//     if(user){
-//       done(null, user);
-//     } else {
-//       done(null, false);
-//     }
-//   });
-// });
+    if(user){
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
 
-// passport.use(localLogin);
-// passport.use(jwtLogin);
+passport.use(localLogin);
+passport.use(jwtLogin);
 
 passport.use(
   new FacebookStrategy({
