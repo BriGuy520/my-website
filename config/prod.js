c60fe0e29c0c6cc6f73ca0088262d4d10af39da0
Brian Falasz <bfalasz@yahoo.com>
2019-06-05 16:07:47 -0500

    initial deployment

--- config/prod.js
+++ config/prod.js
@@ -9,5 +9,6 @@ module.exports = {
   facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
   githubClientID: process.env.GITHUB_CLIENT_ID,
   githubClientSecret: process.env.GITHUB_CLIENT_SECRET,
-  jwtSecret: process.env.JWT_SECRET
+  jwtSecret: process.env.JWT_SECRET,
+  redirectDomain: process.env.REDIRECT_DOMAIN
 }
