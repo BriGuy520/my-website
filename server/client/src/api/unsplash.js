3db26b819d2866f76b6d458d141dcb3f307ccfe6
Brian Falasz <bfalasz@yahoo.com>
2023-01-05 21:20:07 -0600

    deleting stuff

--- server/client/src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization: 'Client-ID 0e3bff419c44f369ed46b8549584422391d59fc842fbe88d5ebf7228874b1d05'
-  }
-});
--- server/client/src/components/blogs/formFields.js
+++
@@ -1,6 +0,0 @@
-export default [
-  { label: "Title", name: "title" },
-  { label: "Image", name: "image"},
-  { label: "Description", name: "description"},
-  { label: "Body", name: "body" }
-];
