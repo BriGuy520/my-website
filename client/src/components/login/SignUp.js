761649f25da8c62de317c8ba8b68782d6bbec650
Brian Falasz <bfalasz@yahoo.com>
2023-03-28 20:23:16 -0500

    changing the styling to the signup form and updating box shadow across the site

--- client/src/components/login/SignUp.js
+++ client/src/components/login/SignUp.js
@@ -18,55 +18,58 @@ class SignUp extends Component {
     const { handleSubmit } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <div className="ui form signup-form">
-          <div className="field">
-              <label>First Name</label>
-              <Field
-                name="firstName"
-                type="text"
-                component="input"
-                autoComplete="none"
-              />
-              <label>Last Name</label>
-              <Field
-                name="lastName"
-                type="text"
-                component="input"
-                autoComplete="none"
-              />
-              <label>Username</label>
-              <Field
-                name="username"
-                type="text"
-                component="input"
-                autoComplete="none"
-              />
-              <label>Password</label>
-              <Field 
-                name="password"
-                type="password"
-                component="input"
-                autoComplete="none"
-              />
-                 <label>Confirm Password</label>
-              <Field 
-                name="confirm-password"
-                type="password"
-                component="input"
-                autoComplete="none"
-              />
-              <div className="signup-btns">
-                <button className="ui button green signup-btn">Sign Up</button>
-                <button className="ui button blue signup-btn"><a href="/">Cancel</a></button>
+      <div className="container">
+        <div className="form-container">
+          <form onSubmit={handleSubmit(this.onSubmit)}>
+            <div className="ui form signup-form">
+              <div className="field">
+                  <label>First Name</label>
+                  <Field
+                    name="firstName"
+                    type="text"
+                    component="input"
+                    autoComplete="none"
+                  />
+                  <label>Last Name</label>
+                  <Field
+                    name="lastName"
+                    type="text"
+                    component="input"
+                    autoComplete="none"
+                  />
+                  <label>Username</label>
+                  <Field
+                    name="username"
+                    type="text"
+                    component="input"
+                    autoComplete="none"
+                  />
+                  <label>Password</label>
+                  <Field 
+                    name="password"
+                    type="password"
+                    component="input"
+                    autoComplete="none"
+                  />
+                    <label>Confirm Password</label>
+                  <Field 
+                    name="confirm-password"
+                    type="password"
+                    component="input"
+                    autoComplete="none"
+                  />
+                  <div className="signup-btns">
+                    <button className="ui button green signup-btn">Sign Up</button>
+                    <button className="ui button blue signup-btn"><a href="/">Cancel</a></button>
+                  </div>
+                <div>
+                  {this.props.errorMessage}
+                </div>
               </div>
-            <div>
-              {this.props.errorMessage}
             </div>
-          </div>
+          </form>
         </div>
-       
-      </form>
+      </div>
     );
   }
 }
